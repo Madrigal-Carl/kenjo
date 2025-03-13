6cf2a7ee401c95d3f860989a6ccf2142b60a9625
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2025-03-13 17:57:52 +0800

    Navbar Protype

--- resources/js/layouts/Navbar.jsx
+++ resources/js/layouts/Navbar.jsx
@@ -1,3 +1,54 @@
+import { Search, ShoppingCart, AccountCircle } from "@mui/icons-material";
+import { Link } from '@inertiajs/react'
+
 export default function Navbar() {
-    return <h1>Navbar</h1>
+    return (
+        <>
+            <div className="bg-[#8B4A24] p-3">
+
+                <div className="flex items-center justify-between max-w-6xl mx-auto px-2">
+
+                    <Link href="/" className="flex items-center">
+                        <img
+                            src="images/kenjo_logo.png"
+                            alt="KenJo Logo"
+                            className="h-14 lg:h-16"
+                        />
+                    </Link>
+
+                    <div className="hidden sm:flex items-center bg-white rounded-full overflow-hidden w-full max-w-lg border-2 border-[#7A3E1D] shadow-md">
+                        <input
+                            type="text"
+                            placeholder="Search products..."
+                            className="flex-1 px-4 py-2 outline-none bg-transparent text-gray-900"
+                        />
+                        <button className="bg-[#7A3E1D] p-2 rounded-r-full hover:bg-[#5F2E15] transition">
+                            <Search className="text-white" />
+                        </button>
+                    </div>
+
+                    <div className="flex items-center space-x-6 text-white">
+                        <a href="/cart">
+                            <ShoppingCart className="cursor-pointer" />
+                        </a>
+                        <AccountCircle className="cursor-pointer" />
+                    </div>
+                </div>
+
+                <div className="sm:hidden mt-3">
+                    <div className="flex items-center bg-white rounded-full overflow-hidden border-2 border-[#7A3E1D] shadow-md mx-2">
+                        <input
+                            type="text"
+                            placeholder="Search products..."
+                            className="flex-1 px-4 py-2 outline-none bg-transparent text-gray-900"
+                        />
+                        <button className="bg-[#7A3E1D] p-2 rounded-r-full hover:bg-[#5F2E15] transition">
+                            <Search className="text-white" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return inertia('client/Home');
 });
+
+Route::get('/cart', function () {
+    return inertia('client/Cart');
+});
